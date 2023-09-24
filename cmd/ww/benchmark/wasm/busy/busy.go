add6955631a672b554012b3bfbc5bd2c92067ee9
mikelsr <mikelsr@pm.me>
2023-09-25 01:21:14 +0200

    Implement basic benchmarking

---
+++ cmd/ww/benchmark/wasm/busy/busy.go
@@ -0,0 +1,42 @@
+//go:generate env GOOS=wasip1 GOARCH=wasm go build -o busy.wasm busy.go
+package main
+
+import (
+	"context"
+	"runtime"
+	"strconv"
+
+	ww "github.com/wetware/pkg/guest/system"
+)
+
+const (
+	TOTAL_CYCLES = iota
+	YIELD_CYCLES
+)
+
+func main() {
+	ctx := context.Background()
+
+	_, err := ww.Bootstrap(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	total, err := strconv.ParseInt(ww.Args()[TOTAL_CYCLES], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	yield, err := strconv.ParseInt(ww.Args()[YIELD_CYCLES], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	var x int64
+	for i := int64(0); i < total; i++ {
+		x++
+		if x%yield == 0 {
+			// TODO this affecst the Wazero runtime, no the Go runtime
+			runtime.Gosched()
+		}
+	}
+}
